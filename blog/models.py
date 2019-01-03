b0d5cfd24577d5b027ac98f9a3848467032d0490
olenakovpak <donogh@ya.ru>
2019-01-03 13:16:47 +0200

    Olena Kovpak's site, fis commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    created_date = models.DateField(default=timezone.now, blank=True,                                                 null=True)
+    published_date = models.DateTimeField(default=timezone.now, blank=True,                                                 null=True)
+    image = models.ImageField(blank=True, null=True)
+    # Add functionality to add videos
+    
+    def publish(self):
+        self.published_date = timezone.now
+        self.save()
+    
+    def __str__(self):
+        return self.title
