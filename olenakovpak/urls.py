d93f9a0fe33a50e6d994f3c1904be0f300713458
olenakovpak <donogh@ya.ru>
2019-01-05 17:09:26 +0200

    Changed template in blog app

--- olenakovpak/urls.py
+++ olenakovpak/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('eventsandworkshops/', include('eventsandworkshops.urls')),
     path('blog/', include('blog.urls')),
     #path('contacts/', include('contacts.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)#
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
