2ac27751636c0e9fa7722857a5017de0443e2011
olenakovpak <donogh@ya.ru>
2019-01-20 23:37:32 +0200

    Changed templates for home, about, works, events & workshops and blog applications

--- works/models.py
+++ works/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
     created_date = models.DateField(default=timezone.now, blank=True, null=True)
     published_date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='static/works', blank=True, null=True)
@@ -14,6 +14,8 @@ class Post(models.Model):
         self.published_date = timezone.now
         self.save()
     
+'''
     def __str__(self):
         if self.title:
             return self.title
+'''
